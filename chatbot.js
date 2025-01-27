26fb1d00b354d1fe2d865a70371ba30144f443b7
Niraj Malviya <131042891+nirajmalviya@users.noreply.github.com>
2025-01-27 23:42:04 +0530

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -14,13 +14,14 @@ document.getElementById("send-btn").addEventListener("click", async () => {
   
     // Call API to get bot response
     try {
- const response = await fetch("https://chat-bot-wtqw.onrender.com/chat", {
+const response = await fetch("https://chat-bot-wtqw.onrender.com/chat", {
   method: "POST",
   headers: {
     "Content-Type": "application/json",
   },
   body: JSON.stringify({ message: userInput }),
 });
+
   
       const data = await response.json();
   
